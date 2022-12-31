f527c6d86bdd6d1a447f243f7b2c897ae61c6424
SantiagoYpes <79435015+SantiagoYpes@users.noreply.github.com>
2022-12-31 01:01:55 -0500

    Eight

--- dist/db.js
+++ dist/db.js
@@ -26,7 +26,7 @@ var _config = require("./config");
         case 8:
           _context.prev = 8;
           _context.t0 = _context["catch"](0);
-          console.error(error);
+          console.error("No db Connection");
         case 11:
         case "end":
           return _context.stop();
